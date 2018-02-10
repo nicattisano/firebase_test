54061b8f54770d27dde513906c3dc88a6d79d90b
Nicole Attisano <nicoleattisano@Nicoles-MacBook-Pro.local>
2018-02-10 18:59:20 -0500

    new data structure

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -6,14 +6,19 @@ class HomePage extends Component {
   constructor(props) {
       super(props);
        this.state = {
-          items: {
-  //            id1: {
-  //                date: 'October 3',
-  //                store: 'Tims',
-  //                price: '$5',
-  //                description: 'blahhh',
-  //                category: 'Entertainment'
-  //            }
+          users: {
+             user_id: {
+                 feb_2018: {
+                   sessions : 4,
+                   page_views : 5
+                 }
+             },
+             user_id_2: {
+                 feb_2018: {
+                   sessions : 6,
+                   page_views : 7
+                 }
+             }
           }
     }
   }
@@ -25,10 +30,10 @@ class HomePage extends Component {
         <h1>Home Page</h1>
 
 
-        { Object.keys(this.state.items).map((id) => {
-          let anItem = this.state.items[id];
+        { Object.keys(this.state.users).map((id) => {
+          let aUser = this.state.users[id];
             return <div key={ id }>
-                {anItem.february.sessions}
+                {aUser.feb_2018.sessions}
               </div>
         })}
 
